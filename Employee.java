2135f8cacb4c287faf8c8fbe565a7d69bbf7b114
Buğra Haskan <bugrahaskan@outlook.com>
2022-10-12 23:05:15 +0300

    practicum case work

---
+++ Employee.java
@@ -0,0 +1,92 @@
+package com.bugra.drafts;
+
+import java.util.Date;
+
+class Employee {
+
+	private String name;
+	private double salary;
+	private int workHours;
+	private int hireYear;
+	
+	Date date = new Date();
+	
+	Employee (String name, double salary, int workHours, int hireYear) {
+		this.name = name;
+		this.salary = salary;
+		this.workHours = workHours;
+		this.hireYear = hireYear;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getWorkHours() {
+		return workHours;
+	}
+	public void setWorkHours(int workHours) {
+		this.workHours = workHours;
+	}
+	public int getHireYear() {
+		return hireYear;
+	}
+	public void setHireYear(int hireYear) {
+		this.hireYear = hireYear;
+	}
+	
+	
+	public double tax() {
+		if (this.salary > 1000.0) {
+			return this.salary * 0.03;
+		} else {
+			return 0.0;
+		}
+	}
+	
+	public double bonus() {
+		if (this.workHours > 40) {
+			return (this.workHours - 40) * 30.0;
+		} else {
+			return 0.0;
+		}
+	}
+	
+	public double raiseSalary() {
+		int duration = date.getYear() + 1900 - hireYear;
+		
+		double totalSalary = ( this.salary - this.tax() + this.bonus() );
+		double raiseSalary = 0.0;
+		
+		if(duration < 10) {
+			raiseSalary = totalSalary * 0.05;
+		} else if (duration > 9 && duration < 20) {
+			raiseSalary = totalSalary * 0.10;
+		} else if (duration > 19) {
+			raiseSalary = totalSalary * 0.15;
+		}
+		
+		return raiseSalary;
+	}
+
+	public void output() {
+		System.out.println("Adı:\t\t\t\t" + this.name);
+		System.out.println("Maaşı:\t\t\t\t" + this.salary);
+		System.out.println("Çalışma saati:\t\t\t" + this.workHours);
+		System.out.println("Başlangıç yılı:\t\t\t" + this.hireYear);
+		System.out.println("Vergi:\t\t\t\t" + this.tax());
+		System.out.println("Bonus:\t\t\t\t" + this.bonus());
+		System.out.println("Maaş artışı:\t\t\t" + this.raiseSalary());
+		System.out.println("Vergi ve Bonus ile Maaş:\t" + (this.salary - this.tax() + this.bonus()));
+		System.out.println("Toplam maaş:\t\t\t" + (this.salary + this.bonus()));
+	}
+	
+}
---
+++ EmployeeTest.java
@@ -0,0 +1,10 @@
+package com.bugra.drafts;
+
+class EmployeeTest {
+
+    public static void main(String args[]) {
+        
+    	Employee bugra = new Employee("Buğra Haskan", 10000.0, 30, 2022);
+        bugra.output();
+    }
+}
