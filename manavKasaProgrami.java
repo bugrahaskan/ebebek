1a7069df21705f969f4b54680da8ad7a4a37a685
Buğra Haskan <bugrahaskan@outlook.com>
2022-10-28 19:03:52 +0300

    Manav kasa programı

---
+++ manavKasaProgrami.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class manavKasaProgrami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double[] fiyat = {2.14, 3.67, 1.11, 0.95, 5.00};
+        double[] tutar = new double[5];
+        System.out.print("Armut kaç kilo?");
+        tutar[0] = input.nextDouble();
+        System.out.print("Elma kaç kilo?");
+        tutar[1] = input.nextDouble();
+        System.out.print("Domates kaç kilo?");
+        tutar[2] = input.nextDouble();
+        System.out.print("Muz kaç kilo?");
+        tutar[3] = input.nextDouble();
+        System.out.print("Patlıcan kaç kilo?");
+        tutar[4] = input.nextDouble();
+
+        double toplam = 0;
+        for (int i = 0; i < 5; i++) {
+            toplam += fiyat[i] * tutar[i];
+        }
+        System.out.println("Toplam Tutar:" + toplam);
+
+    }
+}
